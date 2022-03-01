8a3d9307946b295e6db73d8dfcd6e6a70b5b32b6
Igor <igor.vasiljevic@eurobit.ba>
2022-03-01 04:16:20 +0100

    refactor

--- koa-server/src/api/routers/profile.ts
+++ koa-server/src/api/routers/profile.ts
@@ -1,8 +1,8 @@
-import _ from 'lodash';
 import Router from '@koa/router';
 import authenticated from '../../koa/middleware/authentication';
 import { hasAnyRole } from '../../koa/middleware/authorization';
 import { ROLE_ADMIN, ROLE_USER } from '../../auth/auth-utils';
+import { CustomContext } from '../../koa/types';
 
 /**
  * Checking and returning current user session: if exist return 200 else empty body and 204.
@@ -10,9 +10,9 @@ import { ROLE_ADMIN, ROLE_USER } from '../../auth/auth-utils';
  * @param  {Object}  ctx  the default koa context whose encapsulates
  *                          node's request and response objects into a single object
  */
-async function get(ctx) {
+const get = async (ctx: CustomContext) => {
   const { user } = ctx.session;
-  const userId = _.get(user, 'id');
+  const userId = user?.id;
 
   ctx.status = userId ? 200 : 204;
   ctx.body = { user };
@@ -20,12 +20,12 @@ async function get(ctx) {
   if (userId) {
     const profile = await ctx.redis.hget(`user:${userId}`, 'id');
 
-    if (_.isNull(profile)) {
+    if (!profile) {
       ctx.session = null;
       ctx.throw(403, 'Session expired');
     }
   }
-}
+};
 
 export default new Router()
   .get('/', authenticated(), hasAnyRole(ROLE_USER, ROLE_ADMIN), get);
