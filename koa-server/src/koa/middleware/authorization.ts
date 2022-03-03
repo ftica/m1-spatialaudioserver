6b0ee593a9347c28b5bc13cae58974b03b68620a
Igor <igor.vasiljevic@eurobit.ba>
2022-03-03 21:48:16 +0100

    refactor

--- koa-server/src/koa/middleware/authorization.ts
+++ koa-server/src/koa/middleware/authorization.ts
@@ -3,13 +3,14 @@ import { Next } from 'koa';
 import { Token } from '../../api/services/jwt-service';
 import { CustomContext } from '../types';
 
-export const authorize = (tokenTestFn: (token: Token) => boolean) => async (ctx: CustomContext, next: Next) => {
-  if (!tokenTestFn(ctx.token)) {
-    ctx.throw(403, 'Unauthorized!');
-  }
+export const authorize = (testToken: (token: Token) => boolean) =>
+  async (ctx: CustomContext, next: Next) => {
+    if (!testToken(ctx.token)) {
+      ctx.throw(403, 'Unauthorized!');
+    }
 
-  await next();
-};
+    await next();
+  };
 
 export const hasAnyRole = (...roles: Role[]) =>
   authorize((token: Token) => roles.includes(token.role));
