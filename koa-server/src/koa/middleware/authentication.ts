2604ca3276433f57ee430b3f94ab9f716af4d16a
Igor <igor.vasiljevic@eurobit.ba>
2022-03-08 12:36:17 +0100

    refactor

--- koa-server/src/koa/middleware/authentication.ts
+++
@@ -1,9 +0,0 @@
-// import { Context, Next } from 'koa';
-
-// export default () => async (ctx: Context, next: Next) => {
-//   if (!ctx.token) {
-//     ctx.throw(401, 'Failed to authenticate user');
-//   }
-
-//   await next();
-// };
