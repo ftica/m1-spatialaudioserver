2ab3a9b4aa21fadfbd2028a7be7e4fe63d7cfb02
Igor <igor.vasiljevic@eurobit.ba>
2022-04-23 20:40:04 +0200

    refactor

--- koa-server/src/koa/middleware/cors.ts
+++ koa-server/src/koa/middleware/cors.ts
@@ -1,42 +1,27 @@
 import { Context, Next } from 'koa';
 
-export default () => {
-  const defaultOptions = {
-    allowMethods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowHeaders: ['Content-Type', 'Set-Cookie', 'Cookie', 'Authorization',
-      // 'Sec-Fetch-Dest',
-      // 'Sec-Fetch-Mode',
-      // 'Sec-Fetch-Site',
-      'Tus-Resumable',
-      'Upload-Length',
-      'Upload-Metadata'
-    ]
-  };
+const allowMethods = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'].join(',');
+const allowHeaders = ['Content-Type', 'Authorization'/*, 'Set-Cookie', 'Cookie' */].join(',');
 
-  return async (ctx: Context, next: Next) => {
-    const origin = ctx.get('Origin');
-    ctx.vary('Origin');
+export default () => async (ctx: Context, next: Next) => {
+  const origin = ctx.get('Origin');
+  ctx.vary('Origin');
 
-    if (!origin) {
-      await next();
-      return;
-    }
+  if (!origin) return await next();
 
-    ctx.set('Access-Control-Allow-Origin', origin);
-    ctx.set('Access-Control-Allow-Credentials', 'true');
+  ctx.set('Access-Control-Allow-Origin', origin);
+  ctx.set('Access-Control-Allow-Credentials', 'true');
 
-    if (ctx.method !== 'OPTIONS') {
-      return next().catch((err) => {
-        if (err.headers === null) err.headers = {};
-        err.headers['Access-Control-Allow-Origin'] = origin;
-        // _.set(err, 'headers.Access-Control-Allow-Origin', origin);
-        err.headers['headers.Access-Control-Allow-Credentials'] = 'true';
-        throw err;
-      });
-    }
-    ctx.set('Access-Control-Allow-Methods', defaultOptions.allowMethods.join(','));
-    ctx.set('Access-Control-Allow-Headers', defaultOptions.allowHeaders.join(','));
+  if (ctx.method !== 'OPTIONS') {
+    return await next().catch((err) => {
+      if (err.headers === null) err.headers = {};
+      err.headers['Access-Control-Allow-Origin'] = origin;
+      err.headers['Access-Control-Allow-Credentials'] = 'true';
+      throw err;
+    });
+  }
+  ctx.set('Access-Control-Allow-Methods', allowMethods);
+  ctx.set('Access-Control-Allow-Headers', allowHeaders);
 
-    ctx.status = 204;
-  };
+  ctx.status = 204;
 };
