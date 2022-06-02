b6452068ae257869c4a3fcaebed246501df1bd56
Igor <igor.vasiljevic@eurobit.ba>
2022-06-03 01:24:14 +0200

    refactor

--- koa-server/src/api/services/playlist-service.ts
+++ koa-server/src/api/services/playlist-service.ts
@@ -6,10 +6,9 @@ import trackService from './track-service';
 export class PlaylistService extends ModelService<Playlist, Prisma.PlaylistDelegate<Prisma.RejectOnNotFound | Prisma.RejectPerOperation>> {
   override async updateById(id: string, data: any, select?: any): Promise<any> {
     if (data.tracks) {
-      const tran = await db.$transaction([
-        ...data.tracks.map((track: string, index: number) => trackService.query.update({ where: { id: track }, data: { position: index, playlistId: id } }))
+      await db.$transaction([
+        ...data.tracks.map((track: string, index: number) => trackService.query.update({ select: { position: true }, where: { id: track }, data: { position: index, playlistId: id } }))
       ]);
-      console.log('tran: ', tran);
       return await this.findById(id, select);
     }
     return await super.updateById(id, data, select);
